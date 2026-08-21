6bfa12dabec5f42eba4bb4257b9b23f89c6ff35a
Sasidharan93 <sassie.dharan@gmail.com>
2026-08-21 04:29:39 +0000

    Trivy action version

---
+++ app/eslint.config.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        require: 'readonly',
+        module: 'readonly',
+        process: 'readonly',
+        console: 'readonly',
+        __dirname: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
